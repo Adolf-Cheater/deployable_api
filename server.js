d206ce5b6bb31614abdc8105dc926d58b40b896f
Richie Chenyuqi <42722328+Adolf-Cheater@users.noreply.github.com>
2024-08-05 01:06:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const levenshtein = require('fast-levenshtein');
 const spotDataUpload = require('./spotDataUpload'); // Assuming spotDataUpload.js is in the same directory
